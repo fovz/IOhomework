0521e86c1c1172ebaace14b6a298d2f775aee0f2
fovz <461799009@qq.com>
2018-01-31 19:29:38 +0800

    second

--- src/IOhomework/Io.java
+++ src/IOhomework/Io.java
@@ -5,10 +5,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
+import java.io.Writer;
 
 
 public class Io {
@@ -24,11 +26,11 @@ public class Io {
 	    	   Reader read=new FileReader(file);   
 	    	   BufferedReader buread=new BufferedReader(read);
 	    	   String word=buread.readLine();
-	    	   OutputStream wri=new FileOutputStream(pasteword,true);
+	    	   Writer wri=new FileWriter(pasteword,true);
 	    	   String br="\r\n";
 	    	   while(word!=null) {                   //Ð´ÈëÕ³ÌùÎÄ¼þ
-	    		   wri.write(word.getBytes());
-	    		   wri.write(br.getBytes());
+	    		   wri.write(word);
+	    		   wri.write(br);
 	    		   word=buread.readLine();  
 	    	   }
 	    	   wri.close();
